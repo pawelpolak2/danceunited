79e206b45746b7d3376bfb1c82b188dca8c89ffd
pawelpolak2 <pawel.ulic@gmail.com>
2025-11-16 15:50:52 +0100

    init monorepo

---
+++ packages/app/src/routes/home.tsx
@@ -0,0 +1,11 @@
+import { Welcome } from '../welcome/welcome'
+import type { Route } from './+types/home'
+
+// biome-ignore lint/correctness/noEmptyPattern: this is boilerplate code!
+export function meta({}: Route.MetaArgs) {
+  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }]
+}
+
+export default function Home() {
+  return <Welcome />
+}
